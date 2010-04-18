554640d33fac1c865ea0eea78971ed21cdfe1503
William Woodall <wjwwood@gmail.com>
2010-04-18 22:38:24 +0000

    Setup the ax2550_odom.py node so it will be ready to have the math inserted into it.

--- ax2550/nodes/ax2550_odom.py
+++ ax2550/nodes/ax2550_odom.py
@@ -8,9 +8,30 @@ from rospy.rostime import Time
 from ax2550.msg import Encoder 
 from nav_msg.msg import Odometry
 
-def ax2550listener():
-	rospy.init_node('ax2550odom', anonymous=True)
-	rospy.Subscriber('motor_control_encoders', Encoder, self.encoderDataReceived)
-	
-	self.odom_pub = rospy.Publisher('ax2550_odometry', Odometry)
-	
+odom_pub = None
+
+def encoderDataReceived(data):
+    """Called when encoder data is received"""
+    global odom_pub
+    left = data.left # left encoder ticks
+    right = data.right # right encoder ticks
+    ### Do math here
+    
+    ### Insert math into Odom msg so it can be published
+    odom_msg = Odometry()
+    
+    ### Publishing Odom_msg
+    odom_pub.publish(odom_msg)
+
+def ax2550EncodersListener():
+    """Main loop"""
+    global odom_pub
+    rospy.init_node('ax2550_odom', anonymous=True)
+    rospy.Subscriber('motor_control_encoders', Encoder, self.encoderDataReceived)
+    
+    odom_pub = rospy.Publisher('ax2550_odometry', Odometry)
+    
+    rospy.spin()
+    
+if __name__ == '__main__':
+    ax2550EncodersListener()
