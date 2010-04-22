b89a1863cbf919ece17f9ee77e87d71356eedcc9
Michael Carroll <carroll.michael@gmail.com>
2010-04-22 04:19:46 +0000

    Odometry seems to be working

--- ax2550/nodes/ax2550_odom.py
+++ ax2550/nodes/ax2550_odom.py
@@ -6,44 +6,32 @@ import rospy
 from rospy.rostime import Time
 
 from ax2550.msg import Encoder 
-from nav_msg.msg import Odometry
-from geometry_msg.msg
+from nav_msgs.msg import Odometry
 
 import tf
 import math
 
-previous_time = rospy.Time.now()
-current_time = rospy.Time.now()
-
 odom_pub = None
 odom_broadcaster = None
-
-local_offset = (0,0,0)
-global_offset = (0,0,0)
-
 wheel_base_width = 0.6477 #meters = 25.5 in
 wheel_base_length =  0.9144 #meters =  36 in
 wheel_diameter = 0.3175 #meters = 13 in
 wheel_circum = math.pi * wheel_diameter
 encoder_resolution = 250*4 #motor controller reads * 4 (quadrature)
 prev_theta=0
+prev_x=0
+prev_y=0
 
 def encoderDataReceived(data):
     """Called when encoder data is received"""
     global odom_pub
     global odom_broadcaster
-    
+    global wheel_base_width, wheel_base_length, wheel_diameter, wheel_circum,encoder_resolution
+    global prev_theta,prev_x,prev_y
+
     left = data.left * wheel_circum/encoder_resolution # left encoder ticks
     right = data.right * wheel_circum/encoder_resolution # right encoder ticks
     ### Do math here
-    
-    """if(left == right):
-        x = left * math.sin(theta)
-        y = left * math.cos(theta)
-    else:
-        mult = wheel_base_width/2*(right+left)/(right-left) 
-        x = mult*math.sin((right-left)/wheel_base_width + theta)-math.sin(theta)
-        y = -mult*math.cos((right-left)/wheel_base_width + theta)-math.cos(theta)"""
     
     sbar = (right+left)/2
     theta = (right-left)/(2*wheel_base_width) + prev_theta
@@ -51,22 +39,29 @@ def encoderDataReceived(data):
     y = sbar * math.sin(theta) + prev_y
     
     (prev_x,prev_y,prev_theta) = (x,y,theta)
-    
+ 
+    quat = tf.transformations.quaternion_from_euler(0,0,theta)
+   
     ### Insert math into Odom msg so it can be published
     odom_msg = Odometry()
-    odom_msg.header.frame_id="/world"
-    odom_msg.pose.position.x = x
-    odom_msg.pose.position.y = y
-    odom_msg.pose.position.z = 0
-    odom_msg.pose.orientation.x = 0
-    odom_msg.pose.orientation.y = 0
-    odom_msg.pose.orientation.z = 0
-    odom_msg.pose.orientation.w = 1
-    
+    odom_msg.header.frame_id="world"
+    odom_msg.pose.pose.position.x = x
+    odom_msg.pose.pose.position.y = y
+    odom_msg.pose.pose.position.z = 0
+    odom_msg.pose.pose.orientation.x = quat[0]
+    odom_msg.pose.pose.orientation.y = quat[1]
+    odom_msg.pose.pose.orientation.z = quat[2]
+    odom_msg.pose.pose.orientation.w = quat[3]
     
     ### Publishing Odom_msg
     odom_pub.publish(odom_msg)
 
+    ### Publishing tf broadcaster
+    odom_broadcaster.sendTransform((x,y,0),
+                     tf.transformations.quaternion_from_euler(0,0,theta),
+                     rospy.Time.now(),
+                     "odometry",
+                     "world")
 
 def ax2550EncodersListener():
     """Main loop"""
@@ -74,8 +69,9 @@ def ax2550EncodersListener():
     global odom_broadcaster
     
     rospy.init_node('ax2550_odom', anonymous=True)
-    rospy.Subscriber('motor_control_encoders', Encoder, self.encoderDataReceived)
-    
+    rospy.Subscriber('motor_control_encoders', Encoder, encoderDataReceived)
+    odom_broadcaster = tf.TransformBroadcaster()
+        
     odom_pub = rospy.Publisher('ax2550_odometry', Odometry)
     rospy.spin()
     
