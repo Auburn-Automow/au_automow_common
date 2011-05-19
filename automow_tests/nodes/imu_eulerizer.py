b1621bb4e1890ace3d22d4cf201c6f8b3c1af757
Michael Carroll <carroll.michael@gmail.com>
2011-05-19 16:16:50 -0500

    Made the node do what the name implies.
    Moved the other remapper out into the microstrain node.

--- automow_tests/nodes/imu_eulerizer.py
+++ automow_tests/nodes/imu_eulerizer.py
@@ -8,24 +8,30 @@ from geometry_msgs.msg import Vector3, Quaternion, PoseStamped, Point
 import tf
 import math
 
-global imu_pub
-imu_pub = None
+global euler_pub 
+euler_pub = None
 
 def imuDataReceived(data):
-    global imu_pub
+    global euler_pub 
 
     angles = tf.transformations.euler_from_quaternion(
-            [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])
+            [data.orientation.x, 
+            data.orientation.y, 
+            data.orientation.z, 
+            data.orientation.w])
+    imu_msg = Imu()
+    
+    yaw = wrapToPi(angles[2] - math.pi/2)
+
     msg = Vector3()
     msg.x = angles[0]
     msg.y = angles[1]
     msg.z = angles[2]
-    imu_pub.publish(msg)
+    euler_pub.publish(msg)
 
 if __name__=='__main__':
     rospy.init_node('imu_eulerizer', anonymous=True)
     rospy.Subscriber('/imu/data',Imu,imuDataReceived)
 
-    imu_pub = rospy.Publisher('/imu/euler',Vector3)
-
+    euler_pub = rospy.Publisher('/imu/euler',Vector3)
     rospy.spin()
