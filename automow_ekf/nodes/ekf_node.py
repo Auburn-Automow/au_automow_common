d9939a0b0914a3062c4442b428fe0204e41408b8
William Woodall <wjwwood@gmail.com>
2011-05-23 12:00:48 -0500

    Almost working python-filter

--- automow_ekf/nodes/ekf_node.py
+++ automow_ekf/nodes/ekf_node.py
@@ -6,26 +6,30 @@ from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 from ax2550.msg import StampedEncoders
 
-from tf.transforms import euler_from_quaternion
+from tf.transformations import euler_from_quaternion
+import tf
 
 from automow_ekf import AutomowEKF
+
+import numpy as np
 
 ekf = None
 
 def encodersCallback(data):
     global ekf
-    ekf.timeUpdate(data.left_wheel, data.right_wheel, data.header.stamp.to_secs())
+    u = np.array([data.encoders.left_wheel, data.encoders.right_wheel], dtype=np.double)
+    ekf.timeUpdate(u, data.header.stamp.to_sec())
 
 def imuCallback(data):
     global ekf
-    (yaw,r,p) = euler_from_quaternion(data.orientation)
-    ekf.measurementUpdateAHRS(yaw, data.orientation_covariance[8])
+    (r,p,yaw) = euler_from_quaternion([data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])
+    ekf.measurementUpdateAHRS(yaw)#, data.orientation_covariance[8])
 
 def ekf_node():
     global ekf
     rospy.init_node('ekf_node', anonymous=True)
     
-    ekf = AutomowEKF()
+    ekf = AutomowEKF.fromDefault()
     
     rospy.Subscriber("encoders", StampedEncoders, encodersCallback)
     rospy.Subscriber("imu/data", Imu, imuCallback)
