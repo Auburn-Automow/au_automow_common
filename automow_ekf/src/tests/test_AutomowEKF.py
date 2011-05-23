b9e1834c4326ba6348d7c722e5d709372f4555c0
Michael Carroll <carroll.michael@gmail.com>
2011-05-23 14:12:44 -0500

    Added a testcase for WrapToPi

--- automow_ekf/src/tests/test_AutomowEKF.py
+++ automow_ekf/src/tests/test_AutomowEKF.py
@@ -20,6 +20,24 @@ class TestAutomowEKF():
             assert_array_equal(data['F'],test_filter.F)
             assert_array_equal(data['G'],test_filter.G)
 
+    def test_TimeUpdate(self):
+        for x in range(1,6):
+            filename = "./test_data/TimeUpdateTest" + str(x) + ".mat"
+            data = sio.loadmat(filename,struct_as_record=True)
+            test_filter = ekf(
+                np.squeeze(data['x_hat_i']),
+                data['P'],
+                data['Q'],
+                data['R_gps'],
+                data['R_imu'])
+
+    def test_WrapToPi(self):
+        filename = "./test_data/WrapToPiTest.mat"
+        data = sio.loadmat(filename,struct_as_record=True)
+        data = np.hstack((data['input'],data['output'].T))
+        for (inp,out) in data:
+            assert_almost_equal(ekf.wrapToPi(inp),out,decimal=12)
+
 
 if __name__ == "__main__":
     run_module_suite()
