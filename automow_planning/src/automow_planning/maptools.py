8e24f29960197350afe94971427835a9069bc025
William Woodall <wjwwood@gmail.com>
2011-11-11 15:06:29 -0600

    Moving things around.

--- automow_planning/src/automow_planning/maptools.py
+++ automow_planning/src/automow_planning/maptools.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
 """
-Allows you to invert and then uninvert a set of points
+Contains utilities for working with maps and field shapes.
 """
 
 import numpy as np
+from PIL import Image
 from shapely.geometry import Polygon
 from shapely.ops import polygonize
 from math import cos, sin
+
+def image2array(im):
+    if im.mode not in ("L", "F"):
+        raise ValueError, "can only convert single-layer images"
+    if im.mode == "L":
+        a = np.fromstring(im.tostring(), dtype=np.uint8)
+    else:
+        a = np.fromstring(im.tostring(), dtype=np.float32)
+    a.shape = im.size[1], im.size[0]
+    return a
+
+def array2image(a):
+    if a.typecode() == np.uint8:
+        mode = "L"
+    elif a.typecode() == np.float32:
+        mode = "F"
+    else:
+        raise ValueError, "unsupported image mode %s"%(a.typecode())
+    return Image.fromstring(mode, (a.shape[1], a.shape[0]), a.tostring())
 
 class RotationTransform:
     """Represents a rotational transform"""
---
+++ automow_planning/tests/test_coverage_planner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+This will exercise the coverage planner and its abilities.
+"""
+
+import roslib; roslib.load_manifest('automow_planning')
+
+from automow_planning import maptools
+
+if __name__ == '__main__':
+    pass
