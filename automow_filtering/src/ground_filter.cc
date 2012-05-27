aea3c087040bfd09a25bd1ce7d261d429edd46da
William Woodall <wjwwood@gmail.com>
2012-05-26 22:03:27 -0500

    Finishing up the filtering node

--- automow_filtering/src/ground_filter.cc
+++ automow_filtering/src/ground_filter.cc
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "ros/ros.h"
 #include "tf/transform_listener.h"
 #include "sensor_msgs/PointCloud.h"
@@ -5,6 +7,7 @@
 #include "sensor_msgs/point_cloud_conversion.h"
 
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/ros/conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl/filters/passthrough.h>
@@ -13,81 +16,157 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 
+#include <dynamic_reconfigure/server.h>
+#include "automow_filtering/automow_filteringConfig.h"
+
+using std::string;
 using sensor_msgs::PointCloud;
+using sensor_msgs::PointCloud2;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PCLPointCloud;
+
+namespace my_modes {
+  typedef enum {
+    NONE = 0,
+    SACMODEL_PERPENDICULAR_PLANE = 1
+  } modes_t;
+}
+
+using namespace my_modes;
 
 class GroundFilter {
 private:
   ros::NodeHandle n_;
   tf::TransformListener listener_;
   ros::Subscriber pc_sub_;
-  ros::Publisher pc_pub_;
-  ros::Publisher pc_pub_2;
+  ros::Publisher obstacles_pub_;
+  ros::Publisher nonobstacles_pub_;
+  dynamic_reconfigure::Server<automow_filtering::automow_filteringConfig> server_;
+
+  int mode_;
+  string vehicle_frame_;
+  double min_z, max_z;
+  PCLPointCloud last_pc;
+  bool paused, first_cloud, z_filter, downsample;
+  double resolution;
 
 public:
   GroundFilter(ros::NodeHandle n) : 
-    n_(n)
+    n_(n),
+    mode_(SACMODEL_PERPENDICULAR_PLANE),
+    vehicle_frame_("base_footprint"),
+    min_z(0.1f),
+    max_z(0.75f),
+    paused(false),
+    first_cloud(true),
+    z_filter(true),
+    downsample(false),
+    resolution(0.001f)
   {
-    pc_sub_ =
-      n_.subscribe("/stereo/points", 1, &GroundFilter::pcCallback, this);
-    pc_pub_ = n_.advertise<PointCloud>("/ground_filter/points", 1);
-    pc_pub_2 = n_.advertise<PointCloud>("/ground_filter/points2", 1);
+    pc_sub_ = n_.subscribe("/stereo/points", 1, &GroundFilter::pcCallback, this);
+    obstacles_pub_ = n_.advertise<PointCloud2>("/ground_filter/obstacles", 1);
+    nonobstacles_pub_ = n_.advertise<PointCloud2>("/ground_filter/non_obstacles", 1);
+
+    server_.setCallback(boost::bind(&GroundFilter::onConfigure, this, _1, _2));
+
+    n_.getParam(string("vehicle_fame"), vehicle_frame_);
+    n_.getParam("min_z", min_z);
+    n_.getParam("max_z", max_z);
+  }
+
+  void onConfigure(automow_filtering::automow_filteringConfig &config, uint32_t level) {
+    vehicle_frame_ = config.vehicle_frame;
+    downsample = config.downsample;
+    resolution = config.resolution;
+    z_filter = config.z_filter;
+    min_z = config.min_z;
+    max_z = config.max_z;
+    paused = config.paused;
+    mode_ = config.filtering_mode;
+    if (paused && !first_cloud) {
+      last_pc.header.stamp = ros::Time::now();
+      PCLPointCloud pc = last_pc;
+      processPC(pc);
+    }
   }
 
   void pcCallback(const PointCloud::ConstPtr& pc_in)
   {
-    // Convert the pc to a pc2
-    sensor_msgs::PointCloud2 pc2;
-    sensor_msgs::convertPointCloudToPointCloud2(*pc_in, pc2);
-    // PCL pc used for filtering
-    pcl::PointCloud<pcl::PointXYZ> pc, pc_ground, pc_nonground;
-    // Convert into a PCL pc
-    pcl::fromROSMsg(pc2, pc);
-    // Get tf info
-    tf::StampedTransform sensorToBaseTF;
-    try
-    {
-      listener_.waitForTransform("base_footprint", pc_in->header.frame_id,
-                                 pc_in->header.stamp, ros::Duration(0.2));
-      listener_.lookupTransform("base_footprint", pc_in->header.frame_id,
-                                pc_in->header.stamp, sensorToBaseTF);
+    if (paused) {
+      return;
     }
-    catch (tf::TransformException& e)
-    {
-        ROS_ERROR("%s", e.what());
+    // First transform the point cloud to the vehicle frame
+    PointCloud pc_tf;
+    try {
+      listener_.waitForTransform(vehicle_frame_, pc_in->header.frame_id,
+                                 pc_in->header.stamp, ros::Duration(0.2));
+      listener_.transformPointCloud(vehicle_frame_, *pc_in, pc_tf);
+    }
+    catch (tf::TransformException& e) {
+        ROS_WARN("Failed to transform PointCloud: %s", e.what());
         return;
     }
-    
-    // Convert the tf to an eigen
-    Eigen::Matrix4f sensorToBase;
-    pcl_ros::transformAsMatrix(sensorToBaseTF, sensorToBase);
+    // Then convert it to a PointCloud2 and then a PCL point cloud
+    PointCloud2 pc2_in;
+    sensor_msgs::convertPointCloudToPointCloud2(pc_tf, pc2_in);
+    PCLPointCloud pc;
+    pcl::fromROSMsg(pc2_in, pc);
+    last_pc = pc;
+    if (first_cloud) {
+      first_cloud = false;
+    }
+    processPC(pc);
+  }
 
-    // Transform the pc
-    pcl::transformPointCloud(pc, pc, sensorToBase);
+  void processPC(PCLPointCloud &pc) {
+    ROS_INFO("Here");
+    // Do processing based on the settings
+    if (downsample) {
+      pcl::VoxelGrid<pcl::PointXYZ> vg;
+      vg.setInputCloud(pc.makeShared());
+      vg.setLeafSize(resolution, resolution, resolution);
+      vg.filter(pc);
+    }
+    if (z_filter) {
+      filterByHeight(pc, min_z, max_z);
+    }
+    if (mode_ == SACMODEL_PERPENDICULAR_PLANE) {
+      PCLPointCloud ground, nonground;
+      segmentWithRANSAC(pc, ground, nonground);
+      // Publish the nonground pc to obstacles
+      publishPCLPointCloud(nonground, obstacles_pub_);
+      // Publish ground to the nonobstacles
+      publishPCLPointCloud(ground, nonobstacles_pub_);
+    } else {
+      publishPCLPointCloud(pc, obstacles_pub_);
+    }
+  }
 
-    // set up filter for height range, also removes NANs:
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(-1.0, 2.0);
+  void publishPCLPointCloud(PCLPointCloud &pc, ros::Publisher &pub) {
+    PointCloud2 pc_out;
+    pcl::toROSMsg(pc, pc_out);
+    pc_out.header.frame_id = vehicle_frame_;
+    pub.publish(pc_out);
+  }
 
-    // Filter the pc
-    pass.setInputCloud(pc.makeShared());
-    pass.filter(pc);
-
-    // Now estimate and remove the ground plane
-    pc_ground.header = pc.header;
-    pc_nonground.header = pc.header;
+  void segmentWithRANSAC(PCLPointCloud &pc, PCLPointCloud &ground, PCLPointCloud &nonground) {
+    // Remove data way below or above the ground
+    filterByHeight(pc, min_z, max_z);
+    // Set the headers
+    ground.header = pc.header;
+    nonground.header = pc.header;
+    // Create the coefficients and inliers storage
     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-
+    // Create a SACSeg. object
     pcl::SACSegmentation<pcl::PointXYZ> seg;
     seg.setOptimizeCoefficients(true);
     seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(200);
+    seg.setMaxIterations(400);
     seg.setDistanceThreshold(0.1);
     seg.setAxis(Eigen::Vector3f(0,0,1));
-    seg.setEpsAngle(0.35);
-
+    seg.setEpsAngle(0.15);
     // Filter until the cloud is too small or I "find" the ground plane
     pcl::PointCloud<pcl::PointXYZ> filter_pc(pc);
     pcl::ExtractIndices<pcl::PointXYZ> extract;
@@ -99,40 +178,38 @@ public:
         ROS_WARN("No plane found in the pointcloud!");
         break;
       }
-      if (std::abs(coefficients->values.at(3)) < 0.07) {
-        ROS_INFO("True");
-      } else {
-        ROS_INFO("False");
-      }
+      // if (std::abs(coefficients->values.at(3)) < 0.07) {
+      //   ROS_INFO("True");
+      // } else {
+      //   ROS_INFO("False");
+      // }
       // Get the indices using the extractor
       extract.setInputCloud(filter_pc.makeShared());
       extract.setIndices(inliers);
       extract.setNegative(false);
       pcl::PointCloud<pcl::PointXYZ> cloud_out;
       extract.filter(cloud_out);
-      pc_ground += cloud_out;
+      ground += cloud_out;
       if(inliers->indices.size() != filter_pc.size()){
         extract.setNegative(true);
         pcl::PointCloud<pcl::PointXYZ> cloud_out2;
         extract.filter(cloud_out2);
-        pc_nonground += cloud_out2;
+        nonground += cloud_out2;
         filter_pc = cloud_out2;
       }
       found_ground = true;
     }
+  }
 
-    // Reconvert and publish the pc1
-    PointCloud pc_out;
-    pcl::toROSMsg(pc_nonground, pc2);
-    sensor_msgs::convertPointCloud2ToPointCloud(pc2, pc_out);
-    pc_out.header.frame_id = "base_footprint";
-    pc_pub_.publish(pc_out);
-    // Reconvert and publish the pc1
-    PointCloud pc_out2;
-    pcl::toROSMsg(pc_ground, pc2);
-    sensor_msgs::convertPointCloud2ToPointCloud(pc2, pc_out2);
-    pc_out2.header.frame_id = "base_footprint";
-    pc_pub_2.publish(pc_out2);
+  void filterByHeight(PCLPointCloud &pc, double &min, double &max) {
+    // set up filter for height range, also removes NANs:
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(min, max);
+
+    // Filter the pc
+    pass.setInputCloud(pc.makeShared());
+    pass.filter(pc);
   }
 };
 
